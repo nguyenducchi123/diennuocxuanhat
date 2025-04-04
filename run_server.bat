d8d7eed270495f3e9ee86b663dbd5c5d89cef09a
nguyenducchi <nchi88073@gmail.com>
2025-04-04 22:12:19 +0700

    Final fixes and ready for deployment

--- run_server.bat
+++ run_server.bat
@@ -1,3 +1,6 @@
 @echo off
 cd "E:\python wed\site1"
-"C:\Users\ACER\AppData\Local\Programs\Python\Python312\python.exe" manage.py runserver 127.0.0.1:8000
+.\env10\Scripts\activate.bat
+pip install -r requirements.txt
+python manage.py runserver 127.0.0.1
+pause
